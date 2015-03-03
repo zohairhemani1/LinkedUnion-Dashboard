9a81d88b809bf14ccb92206e761ddce4d88da1c3
arbishpalla <arbishpalla@yahoo.com>
2015-03-03 04:03:00 -0800

    changing index page to news

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-	include 'headers/connect_to_mysql.php';
+		include 'headers/connect_to_mysql.php';
+		include 'headers/_user-details.php';
 ?>
 <!DOCTYPE html>
 <!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
@@ -7,7 +8,7 @@
 <!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
 <!-- BEGIN HEAD -->
 
-<!-- Mirrored from thevectorlab.net/adminlab/editable_table.html by HTTrack Website Copier/3.x [XR&CO'2013], Tue, 04 Nov 2014 07:58:54 GMT -->
+<!-- Mirrored from thevectorlab.net/adminlab/news_details.html by HTTrack Website Copier/3.x [XR&CO'2013], Tue, 04 Nov 2014 08:04:57 GMT -->
 <head>
    <meta charset="utf-8" />
    <title>Index</title>
@@ -16,146 +17,110 @@
    <meta content="" name="author" />
    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
    <link href="assets/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" />
+   <link href="assets/bootstrap/css/bootstrap-fileupload.css" rel="stylesheet" />
    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
    <link href="css/style.css" rel="stylesheet" />
-   <link href="css/custom.css" rel="stylesheet" />
-
    <link href="css/style_responsive.css" rel="stylesheet" />
    <link href="css/style_default.css" rel="stylesheet" id="style_color" />
 
    <link href="assets/fancybox/source/jquery.fancybox.css" rel="stylesheet" />
    <link rel="stylesheet" type="text/css" href="assets/uniform/css/uniform.default.css" />
-
-   <link rel="stylesheet" href="assets/data-tables/DT_bootstrap.css" />
-
 </head>
 <!-- END HEAD -->
 <!-- BEGIN BODY -->
 <body class="fixed-top">
-   <!-- BEGIN HEADER -->
 <?php
 include 'headers/menu-top-navigation.php'; 
-?>      <!-- BEGIN PAGE -->
+?>
+      <!-- BEGIN PAGE -->  
       <div id="main-content">
          <!-- BEGIN PAGE CONTAINER-->
          <div class="container-fluid">
-            <!-- BEGIN PAGE HEADER-->
+            <!-- BEGIN PAGE HEADER-->   
             <div class="row-fluid">
                <div class="span12">
-                   <!-- BEGIN THEME CUSTOMIZER-->
 
-                   <!-- END THEME CUSTOMIZER-->
-                  <!-- BEGIN PAGE TITLE & BREADCRUMB-->     
-                  <h3 class="page-title">
-                     Home
-                     <small>View Home Page</small>
+                  <!-- BEGIN PAGE TITLE & BREADCRUMB-->
+                   <h3 class="page-title">
+                     Index
+                     <small>Home Page</small>
                   </h3>
                    <ul class="breadcrumb">
-
-                       <li><a href="index.php"><i class="icon-home"></i></a><span class="divider-last">&nbsp;</span>
+                        <li>
+                           <a href="index.php"><i class="icon-home"></i></a> <span class="divider">&nbsp;</span>
                        </li>
-                       
-                   </ul>
-                  <!-- END PAGE TITLE & BREADCRUMB-->
+                       <li><a href="#">View</a><span class="divider-last">&nbsp;</span>
+                       </li>
+                                 
+                                          </ul>
+                   <!-- END PAGE TITLE & BREADCRUMB-->
                </div>
             </div>
             <!-- END PAGE HEADER-->
-
-            <!-- BEGIN ADVANCED TABLE widget-->
+            <!-- BEGIN PAGE CONTENT-->
             <div class="row-fluid">
-                <div class="span12">
-                
-               
-                    <!-- BEGIN EXAMPLE TABLE widget-->
-                    <div class="widget">
+               <div class="span12">
+                  <div class="widget">
                         <div class="widget-title">
-                            <h4><i class="icon-reorder"></i>Index Table</h4>
-                            <span class="tools">
-                                <a href="javascript:;" class="icon-chevron-down"></a>
-                            </span>
+                           <h4><i class="icon-home"></i> About Us</h4>
+                           <span class="tools">
+                           <a href="javascript:;" class="icon-chevron-down"></a>
+                           </span>                    
                         </div>
-<div class="widget-body">
-			<div class="btn-group">
-               <a href="insert_form.php"><button type="button" class="btn btn-primary"> Add New <i class="icon-plus"></i> </button></a>
-                              </div>
-                <div class="portlet-body">
-                                
-                                <div id="width" class="space15"></div>
-                                <table class="table table-striped table-hover table-bordered" id="sample_editable_1">
-                                    <thead>
-                                    <tr>
-								 <th style="width:8px;">id</th>
-                                    <th>News</th>
-                                    <th>Status</th>
-                                        <div class="widths">
-                                        <th style="display:none">Edit</th>
-                                        <th style="display:none">Delete</th>
-                                    </div>
-                                    </tr>
-                     
-                                           </thead>
-                                    <tbody>
+                        <div class="widget-body">
+                            <div class="row-fluid blog">
 
-				<tr class=''>
-					<td>1</td>
-					<td>Your News Will goes here</td>                    
-					<td style='width:27%;'><a href='insert_form.php' id='update_button' class='btn btn-success' >
-                    <i class='icon-trash'></i> Update</a>
-					<a href='#' id='delete_button'  class='btn btn-danger'>
-                    <i class='icon-edit'></i> delete</a>
-					<a href='view.php' id='view_button' class='btn btn-info'>
-                    <i class='icon-edit'></i> View</a></td>
-					<td style='display:none'><a class='' href='javascript:;'>Edit</a></td>
-					<td style='display:none'><a class='' href='javascript:;'>Delete</a></td>
-				</tr>
-                                    </tbody>
-                                </table>
+                                 <div class="span12">
+                                    <img src="img/cover/<?php echo $cover ;?>" alt="">
+                                   <div>
+                                   <h3>
+                                   About Us
+                                   </h3>
+                                        <p>
+                                        <?php echo $about_us; ?>
+                                          </p>
+                                                                                                                    <!--end post comments-->
+                                </div>
+                                </div></div>
                             </div>
+
                         </div>
-                    </div> 
-                    <!-- END EXAMPLE TABLE widget-->
-                </div>
-            </div>
-
-            <!-- END ADVANCED TABLE widget-->
-
-            <!-- END PAGE CONTENT-->
+                  </div>
+               </div>
+            <!-- END PAGE CONTENT-->         
          </div>
          <!-- END PAGE CONTAINER-->
       </div>
-      <!-- END PAGE -->
+      <!-- END PAGE -->  
    </div>
    <!-- END CONTAINER -->
    <!-- BEGIN FOOTER -->
-<?php  
+ <?php  
 	include 'headers/footer.php';
 	?>
    <!-- END FOOTER -->
-   <!-- BEGIN JAVASCRIPTS -->
+   <!-- BEGIN JAVASCRIPTS -->    
    <!-- Load javascripts at bottom, this will reduce page load time -->
    <script src="js/jquery-1.8.3.min.js"></script>
-   <script src="assets/bootstrap/js/bootstrap.min.js"></script>   
+   <script src="assets/bootstrap/js/bootstrap.min.js"></script>
    <script src="js/jquery.blockui.js"></script>
    <!-- ie8 fixes -->
    <!--[if lt IE 9]>
    <script src="js/excanvas.js"></script>
    <script src="js/respond.js"></script>
    <![endif]-->
+   <script type="text/javascript" src="assets/chosen-bootstrap/chosen/chosen.jquery.min.js"></script>
    <script type="text/javascript" src="assets/uniform/jquery.uniform.min.js"></script>
-   <script type="text/javascript" src="assets/data-tables/jquery.dataTables.js"></script>
-   <script type="text/javascript" src="assets/data-tables/DT_bootstrap.js"></script>
    <script src="js/scripts.js"></script>
-
-   <script src="js/table-editable.js"></script>
-
    <script>
-       jQuery(document).ready(function() {
-           App.init();
-           TableEditable.init();
-       });
+      jQuery(document).ready(function() {       
+         // initiate layout and plugins
+         App.init();
+      });
    </script>
+   <!-- END JAVASCRIPTS -->   
 </body>
 <!-- END BODY -->
 
-<!-- Mirrored from thevectorlab.net/adminlab/editable_table.html by HTTrack Website Copier/3.x [XR&CO'2013], Tue, 04 Nov 2014 07:58:55 GMT -->
+<!-- Mirrored from thevectorlab.net/adminlab/news_details.html by HTTrack Website Copier/3.x [XR&CO'2013], Tue, 04 Nov 2014 08:04:58 GMT -->
 </html>
