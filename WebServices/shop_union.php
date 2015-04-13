43af644da6c47a48866f1a782360ab9fcb0fefb4
zohairHemani1 <zohairhemani1@gmail.com>
2015-04-13 14:03:12 +0500

    ShopUnion Webservice.

--- WebServices/shop_union.php
+++ WebServices/shop_union.php
@@ -4,9 +4,18 @@
 	
 	$city = $_POST['parameterOne'];
 	
+	$latlong    =   get_lat_long($city); // create a function with the name "get_lat_long" given as below
+	$map        =   explode(',' ,$latlong);
+	$mapLat     =   $map[0];
+	$mapLong    =   $map[1];
+	
+	$mapLat = deg2rad($mapLat);
+	$mapLong = deg2rad($mapLong);
+	
 	$returnArray = array();
-	$query = "SELECT * from shopunion where `app_id` = '$appID' AND `city` like '%$city%'";
-	$result = mysqli_query($con,$query);
+	$query = "SELECT *,acos(sin({$mapLat}) * sin(`latitude`) + cos({$mapLat}) * cos(`latitude`) * cos(`longitude` - ({$mapLong}))) * 6371 as distance FROM shopunion 
+					WHERE acos(sin({$mapLat}) * sin(`latitude`) + cos({$mapLat}) * cos(`latitude`) * cos(`longitude` - ({$mapLong}))) * 6371 <= 1000 AND `app_id` = '{$appID}' order by `distance`";
+	$result = mysqli_query($con,$query) or die("error:");
 	while($row = mysqli_fetch_assoc($result))
 	{
 		$returnArray[] = $row;
@@ -14,12 +23,25 @@
 	
 	echo json_encode($returnArray);
 	
+
+	function get_lat_long($address){
+	
+		$address = str_replace(" ", "+", $address);
+	
+		$json = file_get_contents("http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false&region=$region");
+		$json = json_decode($json);
+	
+		$lat = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lat'};
+		$long = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lng'};
+		return $lat.','.$long;
+	}
+	
 	/*
 	
 	
 	shows shops within 1000 miles 
 	
-	$query = "SELECT *,acos(sin(0.43423933161) * sin(lat) + cos(0.43423933161) * cos(lat) * cos(`long` - (1.1699860019))) * 6371 as distance FROM shopunion WHERE acos(sin(0.43423933161) * sin(lat) + cos(0.43423933161) * cos(lat) * cos(`long` - (1.1699860019))) * 6371 <= 1000 order by distance";
+	$query = "SELECT *,acos(sin(0.43423933161) * sin(latitude) + cos(0.43423933161) * cos(latitude) * cos(`longitude` - (1.1699860019))) * 6371 as distance FROM shopunion WHERE acos(sin(0.43423933161) * sin(latitude) + cos(0.43423933161) * cos(latitude) * cos(`longitude` - (1.1699860019))) * 6371 <= 1000 order by distance";
 	
 	
 	*/
